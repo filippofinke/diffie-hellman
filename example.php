86d36c77915d9db1cb7c87ff3fddf2edd6f5df26
Filippo Finke <filippo.finke@samtrevano.ch>
2019-11-16 14:42:48 +0100

    Update example.php

--- example.php
+++ example.php
@@ -6,16 +6,24 @@ $base = mt_rand();
 echo "Generating generator...".PHP_EOL;
 $generator = DiffieHellman::getRandomGenerator();
 
-echo "Computing data to share (1/2)".PHP_EOL;
+echo "Alice: Computing data to share (1/2)".PHP_EOL;
 $alice = new DiffieHellman($base, $generator, 7);
-$aliceToBob = $alice->toShare();
+$aliceToShare = $alice->toShare();
 
-echo "Computing data to share (2/2)".PHP_EOL;
+echo "Bob: Computing data to share (2/2)".PHP_EOL;
 $bob = new DiffieHellman($base, $generator, 5);
-$bobToAlice = $bob->toShare();
+$bobToShare = $bob->toShare();
 
-echo "Computing shared key...".PHP_EOL;
-$aliceKey = $alice->getSharedKey($bobToAlice);
-$bobKey = $bob->getSharedKey($aliceToBob);
+echo "Alice: Computing shared key...".PHP_EOL;
+$aliceKey = $alice->getSharedKey($bobToShare);
+
+echo "Bob: Computing shared key...".PHP_EOL;
+$bobKey = $bob->getSharedKey($aliceToShare);
+
 echo "Alice key: $aliceKey".PHP_EOL;
 echo "Bob key: $bobKey".PHP_EOL;
+if($aliceKey == $bobKey) {
+    echo "Alice and Bob keys are equals!".PHP_EOL;
+} else {
+    echo "! KEYS MISMATCH".PHP_EOL;
+}
